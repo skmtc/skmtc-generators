a93b4b03505eca2324997108ab08695c61270433
Dmitri Grabov <d.grabov@gmail.com>
2025-08-02 18:41:06 +0100

    skip example collation

--- gen-msw/src/MockRoute.ts
+++ gen-msw/src/MockRoute.ts
@@ -1,6 +1,6 @@
 import type { OperationInsertableArgs, TypeSystemValue } from '@skmtc/core'
 import { MswBase } from './base.ts'
-import { collateExamples, isEmpty } from '@skmtc/core'
+import { isEmpty } from '@skmtc/core'
 import { toTsValue, TsNever } from '@skmtc/gen-typescript'
 
 export class MockRoute extends MswBase {
@@ -16,7 +16,7 @@ export class MockRoute extends MswBase {
 
     const example = Object.values(response?.examples ?? {})[0]?.resolve().value
 
-    this.responseData = example ?? collateExamples({ objectSchema: response?.schema, depth: 0 })
+    this.responseData = example // ?? collateExamples({ objectSchema: response?.schema, depth: 0 })
 
     this.requestBodyType =
       operation.toRequestBody(({ schema }) => {
